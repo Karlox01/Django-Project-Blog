5a7d4465458c0b118e076ff116887b07a89ae1b6
Karlox01 <orlandinikarlo@gmail.com>
2023-08-28 18:26:52 +0000

    add host for heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangito.herokuapp.com', '8000-karlox01-djangoprojectb-u1da0a6a7px.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['djangito-4d1cf30901c8.herokuapp.com', '8000-karlox01-djangoprojectb-u1da0a6a7px.ws-eu104.gitpod.io']
 
 
 # Application definition
