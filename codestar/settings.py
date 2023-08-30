6223a0f6fe78c462aefcb1d5e985fea209c91713
Karlox01 <orlandinikarlo@gmail.com>
2023-08-30 20:01:17 +0000

    final project add message nad like option

--- codestar/settings.py
+++ codestar/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -29,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['djangito-4d1cf30901c8.herokuapp.com', '8000-karlox01-djangoprojectb-u1da0a6a7px.ws-eu104.gitpod.io']
 
@@ -60,6 +61,16 @@ SITE_ID = 1
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
